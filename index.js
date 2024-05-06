0fd1a20135ac5ac85c7c3329342d1aa234c91aae
jxmot <webdev@stupiddog.net>
2024-05-05 19:19:06 -0500

    updated for wx data display

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-// get the module...
 const Log = require('simple-text-log');
 const logOut = new Log(require('./runlogopt.js'));
-const path = require('path');
-const scriptName = path.basename(__filename);
 function log(payload) {
-    logOut.writeTS(`${scriptName} - ${payload}`);
+    logOut.writeTS(`${payload}`);
 };
 
 const nextion = require ('./nextion.js')('COM4', log);
 nextion.updateRTC();
 
+// Events
+const EventEmitter = require('events');
+const evts = new EventEmitter();
+
+const cfg = require('./wxsvc/cfg/wxsvc-cfg.js');
+const wsvc = require('./wxsvc/'+cfg.api);
+wsvc.init(evts, log);
+
+evts.on('WSVC_UPDATE', (m) => {
+    log(`WSVC_UPDATE : ${JSON.stringify(m)}`);
+    nextion.sendCmdStr(`currtemp.txt="${Math.round(m.temp)}"`);
+    nextion.sendCmdStr(`currhum.txt="${Math.round(m.humd)}"`);
+    nextion.sendCmdStr(`tstamp.txt="${m.text.tstamp}"`);
+    nextion.sendCmdStr(`windmsg.txt="${m.text.wmsg}"`);
+});
+
+evts.on('WSVC_FORCST', (m) => {
+    log(`WSVC_FORCST : ${JSON.stringify(m)}`);
+});
+
+evts.emit('WSVC_START');
